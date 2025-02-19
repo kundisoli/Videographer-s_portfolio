7548c1cdca5b266d4e5195f8e3779a1ca3676b95
Lisa Chimunhu <162376884+kundisoli@users.noreply.github.com>
2025-02-19 16:27:56 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Activating Lateral Menu
+
+const showMenu = (toggleId, navId) => {
+    const toggle =  document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+            toggle.classList.toggle('bx-x');
+        })
+    }
+}
+
+showMenu('header-toggle', 'nav-menu');
+
+
+
+const navLink = document.querySelectorAll('.nav-link');
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
